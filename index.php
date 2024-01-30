895ef93447e6079fb3418de92aecc6afe098eae8
gds90 <you@example.com>
2024-01-30 14:47:09 +0100

    Added a construct function in Movie class

--- index.php
+++ index.php
@@ -5,4 +5,11 @@ class Movie
     public $genre;
     public $year;
     public $director;
+
+    function __construct($_title, $_genre, $_year)
+    {
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->year = $_year;
+    }
 }
