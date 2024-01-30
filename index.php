4532c2527b7942aec43839e18d1ad63ae07a73bf
gds90 <you@example.com>
2024-01-30 15:17:32 +0100

    UPDATE added film age in the previous echos

--- index.php
+++ index.php
@@ -22,13 +22,13 @@ class Movie
     }
 }
 // creo 2 istanze della classe Movie
-$movie_1 = new Movie("Pulp Fiction", "Thriller", 1994);
-$movie_2 = new Movie("Inception", "Crime", 2010);
+$movie_1 = new Movie("Pulp Fiction", "thriller", 1994);
+$movie_2 = new Movie("Inception", "crime", 2010);
 
 $movie_1->director = "Quentin Tarantino";
 $movie_2->director = "Christopher Nolan";
 
 // stampo a schermo i valori delle proprietà delle due istanze
-echo $movie_1->title . " è un film del " . $movie_1->year . ", scritto e diretto da " . $movie_1->director . ", genere: " . $movie_1->genre . ".";
+echo $movie_1->title . " è un film del " . $movie_1->year . ", scritto e diretto da " . $movie_1->director . ", genere " . $movie_1->genre . ". Sono passati " . $movie_1->getAge() . " anni da quando è uscito per la prima volta nelle sale cinematografiche.";
 echo "<br><br>";
-echo $movie_2->title . " è un film del " . $movie_2->year . ", scritto e diretto da " . $movie_2->director . ", genere: " . $movie_2->genre . ".";
+echo $movie_2->title . " è un film del " . $movie_2->year . ", scritto e diretto da " . $movie_2->director . ", genere " . $movie_2->genre . ". Sono passati " . $movie_2->getAge() . " anni da quando è uscito per la prima volta nelle sale cinematografiche.";
